feab8361c7c08141043f9e3c9d1287604b83e16b
1skepta <prospaul999@gmail.com>
2025-02-06 00:06:24 +0000

    working

--- basketball project/index.js
+++ basketball project/index.js
@@ -4,13 +4,11 @@ let guestScore = 0;
 function increaseHome(points) {
   homeScore += points;
   document.getElementById("home-score").textContent = homeScore;
-  console.log("Button Clicked");
 }
 
 function increaseGuest(points) {
   guestScore += points;
   document.getElementById("guest-score").textContent = guestScore;
-  console.log("Button Clicked");
 }
 
 function resetScores() {
